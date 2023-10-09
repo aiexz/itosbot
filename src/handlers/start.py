64ef49c656bba6092dd09d94d059539f88687117
Alex <aiexz@yandex.ru>
2023-10-09 23:55:13 +0300

    feat: better /start
    Included with guide and examples

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,6 +1,6 @@
 from aiogram import Router
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 router = Router()
 
@@ -8,5 +8,25 @@ router = Router()
 @router.message(CommandStart())
 async def start(message: Message):
     await message.answer(
-        "Hey, send me photo, video or GIF and I'll convert it to a sticker pack"
+        "Hey, send me photo, video or GIF and I'll convert it to a sticker pack.\nCheck guide for more info",
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[
+                [
+                    InlineKeyboardButton(
+                        text="📖 How to use",
+                        url="https://telegra.ph/Guide-to-itosbot-05-05",
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        text="Result video",
+                        url="https://t.me/addemoji/video_443446876_AzP5m_by_itosbot",
+                    ),
+                    InlineKeyboardButton(
+                        text="Photo",
+                        url="https://t.me/addstickers/emojis_443446876_UZN6c_by_itosbot",
+                    )
+                ],
+            ]
+        ),
     )
