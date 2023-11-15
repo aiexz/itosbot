6a026443acabc204ccedf1fa4913310d58665d54
Alex <aiexz@yandex.ru>
2023-11-15 15:55:52 +0300

    feat: support images as files
    Support files to retain transparency in image

--- src/handlers/images.py
+++ src/handlers/images.py
@@ -13,6 +13,7 @@ router = Router()
 
 
 @router.message(F.photo, flags={"new_stickers": True})
+@router.message(F.document.mime_type("image/png"), flags={"new_stickers": True})
 async def image_converter(message: Message):
     await message.bot.send_chat_action(message.chat.id, "upload_photo")
     photo = await message.bot.download(message.photo[-1])
